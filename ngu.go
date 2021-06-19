0785b4433337fdab601142ac684305e96c6ebf4b
Mister Codo <46073244+MisterCodo@users.noreply.github.com>
2021-06-18 20:11:06 -0400

    ui can pick a location and beacons. One can see the map of the picked location. One can start an optimization.

--- ngu.go
+++ ngu.go
@@ -1,7 +1,157 @@
 package main
 
-import "github.com/MisterCodo/ngu/cmd"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/MisterCodo/ngu/maps"
+	"github.com/MisterCodo/ngu/plugins/beacons"
+	"github.com/maxence-charriere/go-app/v8/pkg/app"
+)
+
+type ngu struct {
+	app.Compo
+	locations  []location
+	beacons    []beacon
+	background string
+}
+
+func (n *ngu) OnMount(ctx app.Context) {
+	n.initNGU(ctx)
+}
+
+func (n *ngu) initNGU(ctx app.Context) {
+	n.locations = []location{
+		{id: 0, label: "tutorialisland", prettyName: "Tutorial Island", selected: true},
+		{id: 1, label: "fleshworld", prettyName: "Flesh World", selected: false},
+		{id: 2, label: "planettronne", prettyName: "Planet Tronne", selected: false},
+		{id: 3, label: "candyland", prettyName: "Candy Land", selected: false},
+		{id: 4, label: "mansionsandmanagers", prettyName: "Mansions & Managers", selected: false},
+	}
+	n.background = "url(/web/tutorialisland.png)"
+	n.beacons = []beacon{
+		{id: 0, label: "box", prettyName: "Box"},
+		{id: 0, label: "knight", prettyName: "Knight"},
+		{id: 0, label: "arrow", prettyName: "Arrow"},
+		{id: 0, label: "wall", prettyName: "Wall"},
+		{id: 0, label: "donut", prettyName: "Donut"},
+	}
+	n.Update()
+}
+
+func (n *ngu) Render() app.UI {
+	return app.Main().
+		Body(
+			// map
+			app.P().
+				Body(
+					app.Div().
+						Style("width", fmt.Sprintf("%vpx", 600)).
+						Style("height", fmt.Sprintf("%vpx", 510)).
+						Style("background", n.background).
+						Style("background-size", "contain").
+						Body(),
+				),
+			// location picker
+			app.P().
+				Body(
+					app.Range(n.locations).Slice(func(i int) app.UI {
+						l := n.locations[i]
+						return app.Div().
+							Body(
+								app.Input().Type("radio").ID(l.label).Name("location").Value(l.label).Checked(l.selected).OnChange(n.changeLocation(l)),
+								app.Label().For(l.label).Text(l.prettyName),
+							)
+					}),
+				),
+			// beacons picker
+			app.P().
+				Body(
+					app.Range(n.beacons).Slice(func(i int) app.UI {
+						b := n.beacons[i]
+						return app.Div().
+							Body(
+								app.Input().Type("checkbox").ID(b.label).Name(b.label).Checked(true).OnChange(n.changeBeacon(b)),
+								app.Label().For(b.label).Text(b.prettyName),
+							)
+					}),
+				),
+			// optimization picker
+			// start optimization
+			app.P().
+				Body(
+					app.Input().Type("submit").Value("Optimize!").OnClick(n.optimize),
+				),
+		)
+}
+
+func (n *ngu) changeLocation(l location) app.EventHandler {
+	return func(ctx app.Context, e app.Event) {
+		fmt.Printf("changed location to %s\n", l.label)
+		n.background = fmt.Sprintf("url(/web/%s.png)", l.label)
+		n.Update()
+	}
+}
+
+func (n *ngu) changeBeacon(b beacon) app.EventHandler {
+	return func(ctx app.Context, e app.Event) {
+		beaconValue := ctx.JSSrc.Get("checked").Bool()
+		fmt.Printf("changed beacon %s to %v\n", b.label, beaconValue)
+		n.Update()
+	}
+}
+
+func (n *ngu) optimize(ctx app.Context, e app.Event) {
+	fmt.Println("Optimize!")
+
+	// hardcoded for debugging, use ngu values instead
+	goal := maps.OptimizationGoal(maps.SpeedGoal)
+	beaconTypes := []beacons.BType{beacons.Box}
+	locationName := "TutorialIsland"
+
+	optimizer, err := maps.NewOptimizer(goal, beaconTypes, locationName)
+	if err != nil {
+		fmt.Printf("could not start optimization: %s", err.Error())
+		return
+	}
+	optimizer.Infinite = false
+
+	fmt.Printf("Running %s optimization of map %s\n\n", goal.String(), locationName)
+
+	m, err := optimizer.Run(false)
+	if err != nil {
+		fmt.Printf("could not run optimization: %s", err.Error())
+		return
+	}
+
+	fmt.Printf("map scored %.2f\n", m.Score)
+}
+
+type location struct {
+	id         int
+	label      string
+	prettyName string
+	selected   bool
+}
+
+type beacon struct {
+	id         int
+	label      string
+	prettyName string
+}
 
 func main() {
-	cmd.Execute()
+	app.Route("/", &ngu{})
+	app.RunWhenOnBrowser()
+	http.Handle("/", &app.Handler{
+		Name:        "Hello",
+		Description: "A NGU Map Optimizer",
+	})
+
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Fatal(err)
+	}
+
+	// cmd.Execute()
 }
