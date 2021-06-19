56b270bb94201263f6bb10d3a0debf2fac1dbceb
Mister Codo <46073244+MisterCodo@users.noreply.github.com>
2021-06-19 09:52:38 -0400

    Adds generated github pages site

---
+++ app-worker.js
@@ -0,0 +1,45 @@
+const cacheName = "app-" + "e26accf920fc00b32c45f4ba3516b55cedd7ab47";
+
+self.addEventListener("install", event => {
+  console.log("installing app worker e26accf920fc00b32c45f4ba3516b55cedd7ab47");
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        "/",
+        "/app.css",
+        "/app.js",
+        "/manifest.webmanifest",
+        "/wasm_exec.js",
+        "/web/app.wasm",
+        "https://storage.googleapis.com/murlok-github/icon-192.png",
+        "https://storage.googleapis.com/murlok-github/icon-512.png",
+        
+      ]);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  console.log("app worker e26accf920fc00b32c45f4ba3516b55cedd7ab47 is activated");
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
--- ngu.go
+++ ngu.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"log"
 	"net/http"
+	"os"
+	"syscall"
 
 	"github.com/MisterCodo/ngu/maps"
 	"github.com/MisterCodo/ngu/plugins/beacons"
 	"github.com/MisterCodo/ngu/plugins/locations"
 	"github.com/maxence-charriere/go-app/v8/pkg/app"
+	"github.com/maxence-charriere/go-app/v8/pkg/cli"
+	"github.com/maxence-charriere/go-app/v8/pkg/errors"
+	"github.com/maxence-charriere/go-app/v8/pkg/logs"
 )
 
 type ngu struct {
@@ -177,15 +182,82 @@ type tile struct {
 	usable int
 }
 
+type options struct {
+	Port int `env:"PORT" help:"The port used to listen connections."`
+}
+
+type githubOptions struct {
+	Output string `cli:"o" env:"-" help:"The directory where static resources are saved."`
+}
+
 func main() {
 	app.Route("/", &ngu{})
 	app.RunWhenOnBrowser()
-	http.Handle("/", &app.Handler{
-		Name:        "Hello",
-		Description: "A NGU Map Optimizer",
-	})
 
-	if err := http.ListenAndServe(":8000", nil); err != nil {
-		log.Fatal(err)
+	ctx, cancel := cli.ContextWithSignals(context.Background(),
+		os.Interrupt,
+		syscall.SIGTERM,
+	)
+	defer cancel()
+	defer exit()
+
+	h := app.Handler{
+		Author:      "Mister Codo",
+		Name:        "NGU Optimizer",
+		Description: "Yet another NGU Industries map optimizer",
+		Title:       "NGU Optimizer",
+	}
+
+	opts := options{Port: 4000}
+	cli.Register("local").
+		Help(`Launches a server that serves the documentation app in a local environment.`).
+		Options(&opts)
+
+	githubOpts := githubOptions{}
+	cli.Register("github").
+		Help(`Generates the required resources to run NGU Optimizer app on GitHub Pages.`).
+		Options(&githubOpts)
+	cli.Load()
+
+	switch cli.Load() {
+	case "local":
+		runLocal(ctx, &h, opts)
+
+	case "github":
+		generateGitHubPages(ctx, &h, githubOpts)
+	}
+}
+
+func runLocal(ctx context.Context, h http.Handler, opts options) {
+	app.Logf("%s", logs.New("starting ngu optimizer app server").
+		Tag("port", opts.Port),
+	)
+
+	s := http.Server{
+		Addr:    fmt.Sprintf(":%v", opts.Port),
+		Handler: h,
+	}
+
+	go func() {
+		<-ctx.Done()
+		s.Shutdown(context.Background())
+	}()
+
+	if err := s.ListenAndServe(); err != nil {
+		panic(err)
+	}
+}
+
+func generateGitHubPages(ctx context.Context, h *app.Handler, opts githubOptions) {
+	if err := app.GenerateStaticWebsite(opts.Output, h); err != nil {
+		panic(err)
+	}
+}
+
+func exit() {
+	err := recover()
+	if err != nil {
+		app.Logf("command failed: %s", errors.Newf("%v", err))
+		os.Exit(-1)
 	}
 }
